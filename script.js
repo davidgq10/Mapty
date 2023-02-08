9be3e3390364812523cb1ffa014d28e00bd9880d
David Garbanzo <dgarbanzo@fascr.com>
2023-02-07 22:37:24 -0600

    added marker and form

--- script.js
+++ script.js
@@ -11,10 +11,61 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-const map = L.map('map').setView([9.903916, -84.049519], 16);
-let userCurrPosition;
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution:
-  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+let map, mapEvent;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      map = L.map('map').setView([latitude, longitude], 16);
+      console.log(map);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+      //Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        //console.log(mapEvent);
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      console.log("User doesn't allow location");
+    }
+  );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  //Display marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout!')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function (e) {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
